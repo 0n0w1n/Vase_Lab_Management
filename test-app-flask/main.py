091fa23e8ef30698af7fc631a32b3f0cc7339a6f
Hugh Jaiden <hugh.j@ku.th>
2026-09-11 12:50:24 +0700

    Add requests list

--- test-app-flask/main.py
+++ test-app-flask/main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from testviews import test_pages
+from views import test_pages
 from extensions import db, migrate # Import ORM and Migration Instances
 import seeds
 
@@ -28,7 +28,7 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
 
-    app.register_blueprint(test_pages, url_prefix="/test")
+    app.register_blueprint(test_pages, url_prefix="/")
 
     seeds.init_app(app)
     return app
